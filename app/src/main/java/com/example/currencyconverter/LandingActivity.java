2adfeed206ae9997f830135cfbd770d164e6e515
Joe-Wehbe <joewehbe@yahoo.com>
2022-09-15 18:33:56 +0300

    commit #5: Wrote code for LandingActivity

--- app/src/main/java/com/example/currencyconverter/LandingActivity.java
+++ app/src/main/java/com/example/currencyconverter/LandingActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LandingActivity extends AppCompatActivity {
@@ -12,7 +14,20 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+    }
 
+    static double lbpAmount = 0;
+
+    public void lbpInput(View v){
+        EditText extractingValue = findViewById(R.id.lbpAmount);
+        String amount = extractingValue.getText().toString();
+        lbpAmount = Double.parseDouble(amount);
+        Toast.makeText(getApplicationContext(), amount, Toast.LENGTH_LONG).show();
+    }
+
+    public double convertLBPtoUSD(){
+        double usdAmount = lbpAmount/40000;
+        return usdAmount;
     }
 
 
--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -24,10 +24,13 @@ public class MainActivity extends AppCompatActivity {
         String password_val = password.getText().toString();
 
         if(username_val.equals("user.leb") && password_val.equals("123456")){
-            Toast.makeText(getApplicationContext(), "You are logged in", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Log in successful", Toast.LENGTH_LONG).show();
             Intent i = new Intent(getApplicationContext(), LandingActivity.class);
             startActivity(i);
-        }else{
+        } if(username_val.equals("") || password_val.equals("")){
+            Toast.makeText(getApplicationContext(), "Missing credentials", Toast.LENGTH_LONG).show();
+        }
+        else{
             Toast.makeText(getApplicationContext(), "Incorrect username or password", Toast.LENGTH_LONG).show();
 
         }
